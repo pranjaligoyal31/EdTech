e85c7e46d5882f3722cbeb7c42d0ae89ad9015c6
pranjaligoyal31 <goyalpranjali09@gmail.com>
2025-05-15 11:21:51 +0530

    Initial commit

---
+++ middlewares/auth.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/User");
+
+//auth
+exports.auth = async (req, res, next) => {
+    try {
+        //extract token
+        const token = req.cookies.token
+                        || req.body.token
+                        || req.header("Authorisation").replace("Bearer", "");
+
+        //if token missing,then return response
+        if(!token) {
+            return res.status(401).json({
+                success:false,
+                message:'Token is missing',
+            });
+        }
+
+        //verify the token
+        try {
+            const decode = await jwt.verify(token, process.env.JWT_SECRET)
+        } catch (error) {
+            
+        }
+    } 
+    catch (error) {
+        
+    }
+}
+
+//isStudent
+
+
+//isInstructor
+
+
+//isAdmin
