f6400b33110e498416d0d8c4866d1bb804922d85
pranjaligoyal31 <goyalpranjali09@gmail.com>
2025-05-15 15:35:18 +0530

    second commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -20,20 +20,76 @@ exports.auth = async (req, res, next) => {
 
         //verify the token
         try {
-            const decode = await jwt.verify(token, process.env.JWT_SECRET)
+            const decode =  jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decode);
+                req.user = decode;
         } catch (error) {
+            //verification - issue
+            return res.status(401).json({
+                success:false,
+                message:'token is invalid',
+            });
             
         }
+        next();//->going on next middleware
     } 
     catch (error) {
-        
+        return res.status(401).json({
+            success:false,
+            message:'Something went wrong while validating the token',
+        });
     }
 }
 
 //isStudent
+exports.isStudent= async (req, res, next) => {
+    try {
+        if(req.user.accountType !== "Student") {
+            return res.status(401).json({
+                success:false,
+                message:'This is a protected route fro Students only',
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:'User role cannot be verified, please try again'
+        })
+    }
+}
 
 
 //isInstructor
+exports.isInstructor= async (req, res, next) => {
+    try {
+        if(req.user.accountType !== "Instructor") {
+            return res.status(401).json({
+                success:false,
+                message:'This is a protected route for Instructor only',
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:'User role cannot be verified, please try again'
+        })
+    }
+}
 
 
 //isAdmin
+exports.isAdmin= async (req, res, next) => {
+    try {
+        if(req.user.accountType !== "Admin") {
+            return res.status(401).json({
+                success:false,
+                message:'This is a protected route for Admin only',
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:'User role cannot be verified, please try again'
+        })
+    }
+}
